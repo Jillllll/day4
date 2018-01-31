e4c0d3aa2309ef2ab853d5aaf8703824d022f49d
jjiao <751682071@qq.com>
2018-01-31 12:59:33 +0800

    static and dynamic fields

--- object/object_test.py
+++ object/object_test.py
@@ -2,7 +2,11 @@
 # _*_coding=utf-8_*_
 
 class Province:
+    # static field:belong to class
+    memo = 'one out of 23 provinces'
+
     def __init__(self, name, capital):
+        # dynamic field:belong to object
         self.Name = name
         self.Capital = capital
 
@@ -10,3 +14,4 @@ class Province:
 js = Province('JiangSu', 'NanJing')
 print js.Name
 print(js.Capital)
+print(Province.memo)
