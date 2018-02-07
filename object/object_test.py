01d3d377bc8916209cf1e242efa09cd6b0d74aa1
jjiao <751682071@qq.com>
2018-02-07 12:48:28 +0800

    static and dynamic function

--- object/object_test.py
+++ object/object_test.py
@@ -2,16 +2,26 @@
 # _*_coding=utf-8_*_
 
 class Province:
-    # static field:belong to class
+    # static field:belong to class;can't access to dynamic field
     memo = 'one out of 23 provinces'
 
     def __init__(self, name, capital):
-        # dynamic field:belong to object
+        # dynamic field:belong to object;can access to static field
         self.Name = name
         self.Capital = capital
+
+    # dynamic function
+    def athletic_meet(self):
+        print self.Name + 'is having a athletic meet'
+
+    @staticmethod
+    def establish():
+        print 'Province establish capital'
 
 
 js = Province('JiangSu', 'NanJing')
 print js.Name
 print(js.Capital)
 print(Province.memo)
+js.athletic_meet()
+Province.establish()
